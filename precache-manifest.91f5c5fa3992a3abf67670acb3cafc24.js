2a9d566c2612d51d56c99cebaf281cc1eeae581d
rogpe <prog_x@hotmail.com>
2020-05-13 18:33:32 -0300

    Updates

---
+++ precache-manifest.91f5c5fa3992a3abf67670acb3cafc24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223be9dc38144122462a57532310c944",
+    "url": "/gripezinha/index.html"
+  },
+  {
+    "revision": "3692b173298d53a75d1b",
+    "url": "/gripezinha/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3692b173298d53a75d1b",
+    "url": "/gripezinha/static/js/2.eab551c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gripezinha/static/js/2.eab551c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838fa67635521206ede3",
+    "url": "/gripezinha/static/js/main.7f52dfc9.chunk.js"
+  },
+  {
+    "revision": "0f01bff2af2c889d3fdc",
+    "url": "/gripezinha/static/js/runtime-main.24912129.js"
+  },
+  {
+    "revision": "2138f0621610e9f1c54fd17873a902e1",
+    "url": "/gripezinha/static/media/kod.2138f062.png"
+  }
+]);
